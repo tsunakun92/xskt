78d3c468182028e0a21f9b2e24d0ee2a13990a4e
tsunakun92 <tailm@bisync.co.jp>
2026-03-05 22:00:14 +0700

    init

--- web/Modules/Admin/config/menu.php
+++ web/Modules/Admin/config/menu.php
@@ -34,26 +34,6 @@ return [
                 'icon'  => 'fas fa-cog',
                 'route' => 'settings.index',
             ],
-            [
-                'label' => 'prefecture',
-                'icon'  => 'fas fa-map-pin',
-                'route' => 'prefectures.index',
-            ],
-            [
-                'label' => 'municipality',
-                'icon'  => 'fas fa-building',
-                'route' => 'municipalities.index',
-            ],
-            [
-                'label' => 'post_number',
-                'icon'  => 'fas fa-envelope',
-                'route' => 'post-numbers.index',
-            ],
-            [
-                'label' => 'personal_access_token',
-                'icon'  => 'fas fa-key',
-                'route' => 'personal-access-tokens.index',
-            ],
         ],
     ],
 ];
